4bc2504b2f706e6c4a89a602811e006312ed85b7
Keng Arakawa <kengarakawa@gmail.com>
2022-02-18 18:20:17 +0700

    Start coding

---
+++ src/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore }  from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDA19tlID31Rb3lhVYSFfZHpLQ0glWpgTM",
+  authDomain: "house-marketplace-app-ddb8f.firebaseapp.com",
+  projectId: "house-marketplace-app-ddb8f",
+  storageBucket: "house-marketplace-app-ddb8f.appspot.com",
+  messagingSenderId: "995456288915",
+  appId: "1:995456288915:web:d44d236d7ce1ad6528a566"
+};
+
+export const db = getFirestore()
